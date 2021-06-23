6cb44fe7632d9526c107fc100806a45ad54c82b5
substancep <peter.buddemeyer@gmail.com>
2021-06-23 13:43:41 -0600

    updated text, images, spacing, header

--- pages/components/NavBar.tsx
+++ pages/components/NavBar.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Link from 'next/link';
 const logo = require('../../images/logo.jpg')
+const sm1 = require('../../images/fb2x.png')
+const sm2 = require('../../images/insta2x.png')
 
 export default class NavBar extends React.Component {
     render() {
@@ -9,7 +11,9 @@ export default class NavBar extends React.Component {
               <div className="nav-wrapper">
               <a href="#">
                 <img className="logo" alt={''} src={logo} />
-              </a>
+                </a>
+                {/**<a href=''><img className="sm1" alt={''} src={sm1} /></a>
+        <a><img className="sm2" alt={''} src={sm2} /></a>**/}
               <ul id="nav-mobile" className="right">
                 <li><a className="nav-item" href="#services">Services</a></li>
                 <li><a className="nav-item" href="#contact">Contact</a></li>
